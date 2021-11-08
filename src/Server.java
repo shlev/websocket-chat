e4db826807328aeb3106dfc14533a57e1c3f6274
shlomi <shl.lev.ks@gmail.com>
2021-11-08 09:52:55 +0200

    init wittcode

---
+++ src/Server.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+        try {
+            while(!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has connected");
+                ClientHandler clientHandler = new ClientHandler(socket);
+
+                Runnable target;
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch (IOException io) {
+
+        }
+    }
+
+    public void closeServerSocket() {
+        try {
+            if (serverSocket!=null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(1234);
+            Server server = new Server(serverSocket);
+            server.startServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
